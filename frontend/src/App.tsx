2005803851e7abf5eebc96f36e19ca36e8a64e2f
Justin-Arakaki <Justiniankoi@gmail.com>
2023-04-06 20:27:53 -0700

    Changed to hash routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,13 +8,6 @@ import Footer from './components/footer/Footer';
 import Box from '@mui/material/Box';
 
 function App() {
-	const tempName = '/nicole-yoshinaga-website';
-	const path = {
-		animation: `${tempName}/animation`,
-		art: `${tempName}/art`,
-		aboutme: `${tempName}/aboutme`,
-	};
-
 	return (
 		<MasterProvider>
 			<Box
@@ -28,13 +21,10 @@ function App() {
 				<Box>
 					<Header />
 					<Routes>
-						<Route
-							path="*"
-							element={<Navigate to={path.animation} replace />}
-						/>
-						<Route path={path.animation} element={<Animation />} />
-						<Route path={path.art} element={<Art />} />
-						<Route path={path.aboutme} element={<AboutMe />} />
+						<Route path="*" element={<Navigate to="/animation" replace />} />
+						<Route path="/animation" element={<Animation />} />
+						<Route path="/art" element={<Art />} />
+						<Route path="/aboutme" element={<AboutMe />} />
 					</Routes>
 				</Box>
 				<Footer />
